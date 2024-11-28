844837473413be5be2971e7b70c9eaddc2594783
pyetra <pyetra.muniz@aluno.senai.br>
2024-11-28 10:08:41 -0500

    commit para salvar alteraçoes do python

--- myedia.py
+++ myedia.py
@@ -1,7 +1,18 @@
-qtddaluno = int(input())
-nome = input()
-n1 = float(input())
-n2 = float(input()) 
-n3 = float(input())
-n4 = float(input())
+print("digite n1:")
+nota1 = float(input())
+print("digite nota2:")
+nota2 =float(input())
+print("digite nota3:")
+nota3 =float(input())
+print("digite nota4:")
+nota4 =float(input())
+
+media =((nota1+nota2+nota3+nota4)/4)
+print("a media é:,media")
+if media >=8:
+    print("voce foi aprovado")
+elif media>=5:
+    print("voce esta na recuperaçao")
+elif media<5:
+    print("voce foi reprovado")
 
