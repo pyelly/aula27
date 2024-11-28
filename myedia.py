bad819d6d8a38742bd09f94b4c6e4fffee557f16
pyetra <pyetra.muniz@aluno.senai.br>
2024-11-28 10:09:22 -0500

    commit para salvar alteraçoes do python

--- myedia.py
+++ myedia.py
@@ -6,8 +6,21 @@ print("digite nota3:")
 nota3 =float(input())
 print("digite nota4:")
 nota4 =float(input())
-
+print("digite a quantidade de faltas do aluno")
+quantidade_de_faltas= int (input())
+#ler falta do aluno
 media =((nota1+nota2+nota3+nota4)/4)
+#media
+
+#alteraçoes
+
+#calculos
+
+#faltas
+
+#notas
+
+#soma
 print("a media é:,media")
 if media >=8:
     print("voce foi aprovado")
